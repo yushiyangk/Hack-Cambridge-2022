910622330fea0adeef0e3e68c13c55238a056473
Yu Shiyang <yu.shiyang@gnayihs.uy>
2022-01-23 04:50:15 +0800

    Kinda working frontend for getting scores

--- api/api.py
+++ api/api.py
@@ -4,6 +4,8 @@ import json
 import flask
 import flask_cors as fc
 
+import query
+
 data = {}
 name = {}
 
@@ -18,12 +20,22 @@ app = flask.Flask(__name__)
 cors = fc.CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/")
+@app.route("/api")
 @fc.cross_origin()
 def hello() -> str:
 	return "Hello"
 
 @app.route("/api/stock/<stock_symbol>", methods=['GET'])
 @fc.cross_origin()
-def get(stock_symbol: int) -> str:
-	return flask.jsonify({'name': name[stock_symbol], 'score': data[stock_symbol]})
+def get_single_stock(stock_symbol: str) -> str:
+	name = query.symbol_to_name(stock_symbol)
+	score = query.get_csrhub_score(name)
+	return flask.jsonify({
+		'name': name,
+		'symbol': stock_symbol,
+		'score': score
+	})
+
+
+def get_alternatives(stock_symbol: str):
+	...
--- web/scripts/query.js
+++ web/scripts/query.js
@@ -1,14 +1,53 @@
+
+
 $(document).ready(function() {
-	ROOT = "http://127.0.0.1:5000/"
+	var ROOT = "http://127.0.0.1:5000/";
+
+
+	var outputIndex = 0;
+
 
 
 	$('#main-form').submit(function(event) {
 		event.preventDefault();
 
 		// Query stock symbol and display score
-		stockSymbol = $('#single-stock-symbol').val().trim().toUpperCase()
-		$.getJSON(ROOT + 'api/stock/' + stockSymbol, function(data) {
-			$('#output').html(data['name'] + ': ' + data['score']);
-		})
+		stockSymbol = $('#stock-symbol-entry').val().trim().toUpperCase();
+		$('#stock-symbol-entry').val('');
+		$('#output').append(makeInitialRow(outputIndex, stockSymbol))
+		$.getJSON(ROOT + 'api/stock/' + stockSymbol, getQueryCallback(outputIndex))
+
+		outputIndex++;
 	});
 });
+
+
+function getQueryCallback(outputIndex) {
+	return function(data) {
+		$('#output').append(fillRow(outputIndex, data));
+	};
+}
+
+
+function makeInitialRow(outputIndex, symbol) {
+	return '<tr id="output-' + outputIndex +'">'
+		+ '<td class="name-cell"><img class="loading" src="images/loading.gif" alt="Loading..." /></td>'
+		+ '<td class"symbol-cell">' + symbol + '</td>'
+		+ '<td class="value-cell"><input class="value-field" type="number" value="0" /></td>'
+		+ '<td class="score-cell"><img class="loading" src="images/loading.gif" alt="Loading..." /></td>'
+		+ '<td class="issues-cell"><img class="loading" src="images/loading.gif" alt="Loading..." /></td>'
+		+ '<td class="alternative-cell"><img class="loading" src="images/loading.gif" alt="Loading..." /></td>'
+		+ '</tr>';
+}
+
+function fillRow(index, data) {
+	console.log(index);
+	$('#output-' + index + ' > .name-cell').html(data['name']);
+	$('#output-' + index + ' > .score-cell').html(data['score']);
+}
+
+function makeRow(data) {
+	return '<tr><td>' + data['name'] + '</td><td>' + data['symbol'] + '</td><td><input class="value-field" type="number" value="0" /></td><td>' + data['score'] + '</td><td></td><td></td></tr>'
+}
+
+// Loading dots: https://tenor.com/view/ellipse-dots-cycle-gif-13427673
