b81ae1d717f4befe96a65ab31017a1e2fe218728
Yu Shiyang <yu.shiyang@gnayihs.uy>
2022-01-23 03:25:52 +0800

    CSS template

--- web/scripts/query.js
+++ web/scripts/query.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	ROOT = "http://127.0.0.1:5000/"
 
 
-	$('#single-form').submit(function(event) {
+	$('#main-form').submit(function(event) {
 		event.preventDefault();
 
 		// Query stock symbol and display score
