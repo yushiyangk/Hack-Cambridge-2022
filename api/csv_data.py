6af147224c5c62102411c0870f643b75d9061c3c
Yu Shiyang <yu.shiyang@gnayihs.uy>
2022-01-23 02:09:16 +0800

    API and requirements.txt changes! Implemented basic website for querying ESG score of stocks
    /api/company/<company_id> became /api/stock/<stock_symbol>

--- api/csv_data.py
+++ api/csv_data.py
@@ -1,7 +1,8 @@
 import csv
 import json
 
-from flask import Flask
+import flask
+import flask_cors as fc
 
 data = {}
 name = {}
@@ -13,12 +14,16 @@ with open('data.csv', 'r', newline='') as csv_file:
 		data[row[0]] = row[2]
 		name[row[0]] = row[1]
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+cors = fc.CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@fc.cross_origin()
 def hello() -> str:
 	return "Hello"
 
-@app.route("/company/<company_id>")
-def get(company_id: int) -> str:
-	return f"{name[company_id]} {data[company_id]}"
+@app.route("/api/stock/<stock_symbol>", methods=['GET'])
+@fc.cross_origin()
+def get(stock_symbol: int) -> str:
+	return flask.jsonify({'name': name[stock_symbol], 'score': data[stock_symbol]})
