f344a05aa477aa50187e5f881d1b8c94993b29cb
Felicia Tai <feliciatyf@gmail.com>
2022-01-22 20:51:07 +0000

    name_to_csrname function

--- api/query.py
+++ api/query.py
@@ -5,6 +5,16 @@ def symbol_to_name(symbol):
     result = subprocess.run(['bash', 'name.sh', symbol], stdout=subprocess.PIPE)
     return result.stdout.decode().strip()
 
+def name_to_csrname(name):
+    words = name.split()
+    index = len(words)
+    for i, word in enumerate(words):
+        if i >= 1 and "." in word:
+            index = i
+            break
+    # Join using hyphen as this is what is accepted for CSRHub
+    csrname = "-".join(words[:index])
+    return csrname
 
 def get_similar_stocks(symbol):
     result = subprocess.run(['bash', 'similar.sh', symbol], stdout=subprocess.PIPE)
@@ -18,15 +28,8 @@ def get_similar_stocks(symbol):
 def get_csrhub_score(name):
     # Discard string once we have a word that has a period in it
     # E.g. Alphabet Inc. Cl A becomes Alphabet
-    words = name.split()
-    index = len(words)
-    for i, word in enumerate(words):
-        if i >= 1 and "." in word:
-            index = i
-            break
-    # Join using hyphen as this is what is accepted for CSRHub
-    name = "-".join(words[:index])
-    result = subprocess.run(['bash', 'csrhub.sh', name], stdout=subprocess.PIPE)
+    csrname = name_to_csrname(name)
+    result = subprocess.run(['bash', 'csrhub.sh', csrname], stdout=subprocess.PIPE)
     return int(result.stdout.decode())
 
 
