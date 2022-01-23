574cf6eab8d536268d85abd183d0213799564237
Felicia Tai <feliciatyf@gmail.com>
2022-01-23 00:26:24 +0000

    add get_industries_and_ranking function

--- api/query.py
+++ api/query.py
@@ -87,3 +87,34 @@ def get_PE_ratio(symbol):
     PE_ratio = PE_thing.find('span', {"class" : "primary"}).text
     
     return float(PE_ratio)
+
+def get_industries_and_ranking():
+    '''
+    Scrapes https://www.investors.com/news/esg-companies-best-esg-stocks-industries/ for list of industries and rankings
+    Returns dictionary with keys as industries and top 3 companies per industry
+    Output:
+        {"Industry": [(1st name, 1st symbol), (2nd name, 2nd symbol), (3rd name, 3rd symbol)], ...}
+    '''
+    
+    URL = "https://www.investors.com/news/esg-companies-best-esg-stocks-industries/" 
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+
+    page = requests.get(URL, headers=headers)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find('div', {"class":"main-content-column"})
+    tablesInd = soup.findAll('table')
+    industries = {}
+
+    for industry in tablesInd:
+        entries = industry.findAll('tr')
+        industry_name = entries[0].find('h2').text
+        company_info = []
+        for i in range(2,5):
+            details = entries[i].findAll('td')
+            company_name = details[1].text
+            company_symbol = details[2].find('a').text
+            company_info.append((company_name, company_symbol))
+        industries[industry_name] = company_info
+
+    return industries
