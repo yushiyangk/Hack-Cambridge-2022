6abba6b59e6ee9bda32bfa0ad8cb3fa30c090ef2
Felicia Tai <feliciatyf@gmail.com>
2022-01-23 09:58:27 +0000

    change from tuple to dict

--- api/query.py
+++ api/query.py
@@ -114,7 +114,7 @@ def get_industries_and_ranking():
             details = entries[i].findAll('td')
             company_name = details[1].text
             company_symbol = details[2].find('a').text
-            company_info.append((company_name, company_symbol))
+            company_info.append({'name':company_name, 'symbol': company_symbol})
         industries_list.append({'id': id, 'name': industry, 'top3': company_info})
 
     return industries_list
